f1de7a5dd62e5c3c9ccf074e4b156967c8c979e4
Carluuz <carzz96@hotmail.com>
2023-10-20 14:50:28 +0100

    Changes to be committed: 	modified:   drum-machine/src/App.js 	modified:   drum-machine/src/audio/audioData.js

--- drum-machine/src/App.js
+++ drum-machine/src/App.js
@@ -1,22 +1,51 @@
-import logo from './logo.svg';
 import './App.css';
+//import Q from './audio/audioData';
+import { Q, W, E, A, S, D, Z, X, C } from './audio/audioData';
 
 function App() {
   return (
     <div className="App">
 hi me
       <div id="display"></div>
-      <div className="drum-pad" id="clip1">Q</div>
-      <div className="drum-pad" id="clip2">W</div>
-      <div className="drum-pad" id="clip3">E</div>
-      <div className="drum-pad" id="clip4">A</div>
-      <div className="drum-pad" id="clip5">S</div>
-      <div className="drum-pad" id="clip6">D</div>
-      <div className="drum-pad" id="clip7">Z</div>
-      <div className="drum-pad" id="clip8">X</div>
-      <div className="drum-pad" id="clip9">C</div>
+      <button className="drum-pad" id="clip1">
+        <Q />
+        Q
+      </button>
+      <button className="drum-pad" id="clip2">
+        <W />
+        W
+      </button>
+      <button className="drum-pad" id="clip3">
+        <E />
+        E
+      </button>
+      <button className="drum-pad" id="clip4">
+        <A />
+        A
+      </button>
+      <button className="drum-pad" id="clip5">
+        <S />
+        S
+      </button>
+      <button className="drum-pad" id="clip6">
+        <D />
+        D
+      </button>
+      <button className="drum-pad" id="clip7">
+        <Z />
+        Z
+      </button>
+      <button className="drum-pad" id="clip8">
+        <X />
+        X
+      </button>
+      <button className="drum-pad" id="clip9">
+        <C/>
+        C
+      </button>
     </div>
   );
 }
 
 export default App;
+
---
+++ drum-machine/src/audio/audioData.js
@@ -0,0 +1,101 @@
+import React from 'react';
+
+function Q() {
+  return (
+    <audio 
+      src="https://s3.amazonaws.com/freecodecamp/drums/Heater-1.mp3"
+      className="clip"
+      id="Q"
+    />
+  );
+}
+
+
+function W() {
+    return (
+      <audio 
+        src="https://s3.amazonaws.com/freecodecamp/drums/Heater-2.mp3"
+        className="clip"
+        id="W"
+      />
+    );
+  }
+
+
+  function E() {
+    return (
+      <audio 
+        src="https://s3.amazonaws.com/freecodecamp/drums/Heater-3.mp3"
+        className="clip"
+        id="E"
+      />
+    );
+  }
+
+
+  function A() {
+    return (
+      <audio 
+        src="https://s3.amazonaws.com/freecodecamp/drums/Heater-4_1.mp3"
+        className="clip"
+        id="A"
+      />
+    );
+  }
+
+
+  function S() {
+    return (
+      <audio 
+        src="https://s3.amazonaws.com/freecodecamp/drums/Heater-6.mp3"
+        className="clip"
+        id="S"
+      />
+    );
+  }
+
+
+  function D() {
+    return (
+      <audio 
+        src="https://s3.amazonaws.com/freecodecamp/drums/Dsc_Oh.mp3"
+        className="clip"
+        id="D"
+      />
+    );
+  }
+
+
+  function Z() {
+    return (
+      <audio 
+        src="https://s3.amazonaws.com/freecodecamp/drums/Kick_n_Hat.mp3"
+        className="clip"
+        id="Z"
+      />
+    );
+  }
+
+
+  function X() {
+    return (
+      <audio 
+        src="https://s3.amazonaws.com/freecodecamp/drums/RP4_KICK_1.mp3"
+        className="clip"
+        id="X"
+      />
+    );
+  }
+
+
+  function C() {
+    return (
+      <audio 
+        src="https://s3.amazonaws.com/freecodecamp/drums/Cev_H2.mp3"
+        className="clip"
+        id="C"
+      />
+    );
+  }
+                
+export { Q, W, E, A, S, D, Z, X, C };
